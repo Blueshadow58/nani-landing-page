c228401b21f592bf60c5e0266ad4761d3fe0d681
Blueshadow58 <gamonalfranco@gmail.com>
2022-12-29 20:13:34 -0300

    [feature] contact screen

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { styles } from './styles'
+
 
 export default function Contact() {
 
@@ -8,52 +9,113 @@ export default function Contact() {
 
 
 
+  return (
+    <>
+    <div className=" d-flex flex-column min-vh-100 mt-auto  p-0 m-0 bg-light ">
 
+      <Container>
+        <Row className='text-lg-start text-center  '>
+          <Col lg={12} sm={12} className='py-5 '>
+            <span className="display-5 " style={styles.title} >Contacto</span>  
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='' style={styles.formContainer}>
+              <form>
+                <Row>                
+                    <Col lg={5} sm={12} className='py-2'>                    
+                      <div class="form-floating">
+                        <textarea style={styles.textArea} class="form-control" id='floatingTextarea' placeholder='Escriba su mensaje'></textarea>
+                        <label className='text-muted fs-5' for="floatingTextarea">Escriba su mensaje</label>
+                      </div>              
+                    </Col>
+                    <Col lg={5} sm={12} className='py-2 ps-lg-5'>                  
+                      <div class="mb-4 mt-5">                        
+                        <input type="text" style={styles.input} class="form-control" placeholder='Asunto:'  />                        
+                      </div>
+                      <div class="mb-4">                        
+                        <input type="text" style={styles.input} class="form-control" placeholder='Nombre:'   />
+                      </div>                      
+                      <div class="mb-4">                        
+                        <input type="text" style={styles.input} class="form-control" placeholder='Telefono:' />                        
+                      </div>
+                      <div class="mb-5">                        
+                        <input type="text" style={styles.input} class="form-control" placeholder='Email:'/>
+                      </div>                      
+                      <div className='text-center'>
+                        <Button type="submit" className='px-5' style={styles.purpleButton} class="btn">Enviar</Button>                  
+                      </div>
+                                                              
+                    </Col>  
 
+                    
+                      <img className=' d-none d-xxl-block' style={styles.contactUnicorn}  src={require('../../images/Unicornio scooter.png')}  alt='img'></img>  
+                    
+                
+              {/* Img of the Unicorn */}
+              {/* <div style={styles.containerImg} className=''> */}
+                                
+                        {/* </div>   */}
+                    
+                </Row> 
+                
+              </form>
+           
+              
+            </div>          
+          </Col> 
+                              
+        </Row>  
 
-
-
-
-
-  const Footer = () => (
+          
+                    
+      </Container>
+            
     
-    <Row className="d-flex mt-auto min-vw-100 bg-dark">
-      <span className="text-muted">Place sticky footer content here.</span>
-      <div className="container">
-        
-      </div>
-    </Row>
+          
+
+      
+    </div>
+    <Footer/>
+    </>
   )
 
 
-
-
-
-  return (
-    <div className=" d-flex bd-highlight flex-column min-vh-100 mt-auto  p-0 m-0 ">
-
-
-            
-            {/* <span class=" text-muted">Place sticky footer content here.</span> */}
-          
-
-            <div className='d-flex flex-row align-items-center justify-content-center mt-auto my-5'>
-              <div className="d-flex flex-column text-center w-25">
-                <div>
-                  <img src={require('../../images/Logo Rosa.png')} height={110} alt='img'></img>
-                </div>
-                <div className="py-2 text-center">
-                  <img src={require('../../images/Logotipo - Negro nani.png')} height={25} alt='img'></img>
-                </div>
-                <div className="text-center">
-                  <span className="" style={styles.secondarySpan} >Marketing Digital</span>                
-                </div>
-              </div>
-
-              <span className="text-center w-25">Place sticky footer content here.</span>
-              <span className="text-center w-25">Place sticky footer content here.</span>
-            </div>
-
+  
+  function Footer ()  {
+    return (
+    <div className=" d-flex  flex-column min-vh-25 mt-auto  p-0 m-0 ">
+         <Row className=' align-items-center justify-content-center my-5 p-0 m-0 '>
+                <Col lg={4} sm={12} className="text-center ">
+                  <div>
+                    <img src={require('../../images/Logo Rosa.png')} height={110} alt='img'></img>
+                  </div>
+                  <div className="py-3 text-center">
+                    <img src={require('../../images/Logotipo - Negro nani.png')} height={25} alt='img'></img>
+                  </div>
+                  <div className="text-center">
+                    <span className="h5" style={styles.secondarySpan} >Marketing Digital</span>                
+                  </div>
+                </Col>
+              <Col lg={4} sm={12} className=' text-center py-2'>
+                <span className="h5 ">Asesoria en Marketing de contenidos</span>
+              </Col>
+              <Col lg={4} sm={12} className='text-lg-start text-center  '>              
+                <span class="d-block p-2 ">
+                  <img src={require('../../images/social-icons/Instagram.png')} height={35} alt='img'></img>
+                  <span className='ms-3 h5'>@holasoynani.mkt</span> 
+                </span>
+                <span class="d-block p-2 ">
+                  <img src={require('../../images/social-icons/Vector.png')} height={22} className='ps-1' alt='img'></img>
+                  <span className='ms-4 h5'>holasoynani.mkt@gmail.com</span> 
+                </span>
+                <span class="d-block p-2 ">
+                  <img src={require('../../images/social-icons/WhatsApp.png')} height={30} className='ps-1' alt='img'></img>
+                  <span className='ms-3 h5'>+54 9 3885 72-6293</span>
+                </span>
+              </Col>                                
+            </Row>
             <div className="w-100 px-5 ">
               <footer className=" flex-wrap justify-content-between align-items-center py-1 my-2 border-top border-dark">
                 <div className="col-md-12 d-flex align-items-center justify-content-center">                  
@@ -61,10 +123,9 @@ export default function Contact() {
                 </div>
               </footer>      
             </div>
-          
+      </div>
+    )}
 
-      
-    </div>
-  )
+
 }
 
--- src/components/contact/styles.js
+++ src/components/contact/styles.js
@@ -4,5 +4,57 @@ export const styles = {
     secondarySpan: {
         color: COLORS.secondary,        
     },
+    title: {        
+        color: COLORS.secondary,
+        boxShadow: '0px 10px 15px -20px #AEAEA6',
+        fontWeight: '700',
+    },
+    formContainer: {
+        padding: '2.5rem',
+        backgroundColor: 'rgba(151, 71, 255, 0.5)',        
+        borderRadius: '31px',                
+    },
+    textArea: {        
+        resize: 'vertical', 
+        // minHeight: '45vh',
+        height: '25rem',
+        // maxHeight: '45vh',
+        width: '100%',
+        paddingTop: '2.1rem',
+    },
+    purpleButton: {
+        background: '#9747FF',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.15)',
+        borderRadius: '10px', 
+        fontSize: '1.1rem',       
+    },
+    input: {
+        color: 'back',
+        fontSize: '1.1rem',
+        fontWeight: '500',        
+    },
+    containerImg: {        
+        overflow:'hidden',           
+        // display:'inline-block',
+        // position:'relative',
+        // verticalAlign:'middle',        
+    },
+    contactUnicorn: {        
+        position:'absolute',
+        // display:'flex',        
+        right:'0vh',       
+        maxHeight:'70vh',
+        maxWidth:'70vh',
+        // height:'70vh',
+        // width:'70vh',
+        // marginTop:'80px', 
+       
+        
+        
+    },
+
+    
+
+    
 
 }
